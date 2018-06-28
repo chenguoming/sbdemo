9ddd68e8510f15bf532a9f52ce9ae4625effc9a6
chenguoming <1191392830@qq.com>
2018-06-28 17:23:00 +0800

    shrio

--- src/main/java/com/adamai/domain/SysUser.java
+++ src/main/java/com/adamai/domain/SysUser.java
@@ -1,11 +1,16 @@
 package com.adamai.domain;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -19,32 +24,31 @@ import org.hibernate.annotations.GenericGenerator;
 public class SysUser implements Serializable{
 	//springdatajpa通过注解方式自动生成表时字段的顺序问题
 	@Id
-	@GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
-	private String id ;
-	
+	@GeneratedValue
+	private Integer uid ;
 	//用户名
 	@Column(length=16)
 	private String name ;
-	
 	//密码
 	@Column(length=64)
 	private String pwd ;
-	
 	//状态
 	@Column(length=1)
 	private int status ;
-	
 	//创建时间
 	@Column(length=19)
 	private String createTime ;
+	
+	@ManyToMany(fetch= FetchType.EAGER)//立即从数据库中进行加载数据;
+    @JoinTable(name = "SysUserRole", joinColumns = { @JoinColumn(name = "uid") }, inverseJoinColumns ={@JoinColumn(name = "roleId") })
+    private List<SysRole> roleList;// 一个用户具有多个角色
 
-	public String getId() {
-		return id;
+	public Integer getUid() {
+		return uid;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setUid(Integer uid) {
+		this.uid = uid;
 	}
 
 	public String getName() {
@@ -78,6 +82,15 @@ public class SysUser implements Serializable{
 	public void setCreateTime(String createTime) {
 		this.createTime = createTime;
 	}
+
+	public List<SysRole> getRoleList() {
+		return roleList;
+	}
+
+	public void setRoleList(List<SysRole> roleList) {
+		this.roleList = roleList;
+	}
+
 	
 	
 }
